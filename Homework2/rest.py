e39987a4f82e82e72816f8471c59dd04736dc212
BogdanLionte <bogdan_lionte@yahoo.com>
2019-03-14 09:03:25 +0200

    progress

--- Homework2/rest.py
+++ Homework2/rest.py
@@ -1,49 +1,85 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from sqlite3 import connect
 
-class Student:
-    def __init__(self, IDD):
-        self.IDD = IDD
-        self.name = "Bogdan"
+
+
+
+
+
+
+
+
 
 class RequestsHandler(BaseHTTPRequestHandler):
 
+    def __init__(self, request, client_address, server):
+        super().__init__(request, client_address, server)
+        self.conn = None
+
+
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        message = ""
-
-        if self.path == "/students":
-                message += "\n"
-
-
-        if "/student/" in self.path:
-            print("dA")
-            IDD = self.path.split("/")[2]
-
-        self.wfile.write(str(message).encode())
-        return
-
-    def do_PATCH(self):
-
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Patch")
+        self.handle_get()
 
     def do_POST(self):
-        self.send_response(200)
-        self.end_headers()
+        self.handle_post()
 
-        if "/student/" in self.path:
-            IDD = self.path.split("/")[2]
-            student = Student(IDD)
-            student.name = "nou"
-        self.wfile.write(b"Post")
+    def do_PUT(self):
+        self.handle_put()
 
+    def do_DELETE(self):
+        self.handle_delete()
+
+
+    def handle_get(self):
+
+
+        if self.path == "/customers":
+            self.send_response(200)
+            self.end_headers()
+            cursor = self.conn.cursor()
+            cursor.execute("select * from customers")
+            for row in cursor:
+                self.wfile.write(str(row).encode())
+
+            return
+
+        if "/customers/" in self.path:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"getting customer " + self.path.split("/")[2].encode())
+
+        if self.path == "/products":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"getting all products")
+            return
+
+        if "/products/" in self.path:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"getting product " + self.path.split("/")[2].encode())
+
+    def handle_post(self):
+        pass
+
+    def handle_put(self):
+        pass
+
+    def handle_delete(self):
+        pass
+
+
+def json_factory(cursor, row):
+    json = {}
+    for _id, col in enumerate(cursor.description):
+        json[col[0]] = row[_id]
+    return json
 
 
 def run(server_class=HTTPServer, handler_class=RequestsHandler):
-    conn = connect(r"C:\users\Bogdan\Desktop\bd")
+    conn = connect(r"C:\Projects\CloudComputing\Homework2\bd")
+    conn.row_factory = json_factory
+    handler_class.conn = conn
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
